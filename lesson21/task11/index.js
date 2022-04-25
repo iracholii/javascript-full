add72fa7a9023c5d278581a8e85e21de61de60f5
iracholii <ira.cholii.m@gmail.com>
2022-04-25 18:43:34 +0300

    adding

---
+++ lesson21/task11/index.js
@@ -0,0 +1,17 @@
+export const manageClasses = () => {
+  const oneElem = document.querySelector('.one');
+  oneElem.classList.add('selected');
+
+  const twoElem = document.querySelector('.two');
+  twoElem.classList.remove('selected');
+
+  const threeElem = document.querySelector('.three');
+  threeElem.classList.toggle('three_done');
+
+  const fourElem = document.querySelector('.four');
+  if (fourElem.classList.contains('some-class')) {
+    fourElem.classList.add('another-class');
+  }
+};
+
+manageClasses();
