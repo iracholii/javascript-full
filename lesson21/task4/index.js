d35171fd30efa4882b2c7abfcd7351a8d2c584d4
iracholii <ira.cholii.m@gmail.com>
2022-04-25 16:31:25 +0300

    adding

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,25 +1,25 @@
-const getTitle = () => {
+export const getTitle = () => {
   const titleElem = document.querySelector('.title');
   return titleElem.textContent;
 };
 
 getTitle();
 
-const getDescription = () => {
+export const getDescription = () => {
   const titleElem = document.querySelector('.about');
   return titleElem.innerText;
 };
 
 getDescription();
 
-const getPlans = () => {
+export const getPlans = () => {
   const titleElem = document.querySelector('.plans');
   return titleElem.innerHTML;
 };
 
 getPlans();
 
-const getGoal = () => {
+export const getGoal = () => {
   const titleElem = document.querySelector('.goal');
   return titleElem.outerHTML;
 };
