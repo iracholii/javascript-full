6a6067bf25910c77e0163ba0fa097b6c0974c633
iracholii <ira.cholii.m@gmail.com>
2022-04-26 17:52:25 +0300

    adding

--- lesson22/task6/index.js
+++ lesson22/task6/index.js
@@ -1,5 +1,6 @@
-export const setButton = buttonText => {
-  const bodyElem = document.querySelector('body');
-  bodyElem.innerHTML = `<button>${buttonText}</button>`;
-  console.dir(bodyElem);
+const button = document.querySelector('.single-use-btn');
+const buttonFunk = () => {
+  console.log('clicked');
+  button.remove('click', buttonFunk);
 };
+button.addEventListener('click', buttonFunk);
