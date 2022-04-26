87fca456edac5b88aec8a663c8998844b2bd52d4
iracholii <ira.cholii.m@gmail.com>
2022-04-26 18:42:53 +0300

    adding

--- lesson22/task9/index.js
+++ lesson22/task9/index.js
@@ -1,20 +1,5 @@
-export const finishList = () => {
-  const list = document.querySelector('.list');
-
-  const elem1 = document.createElement('li');
-  elem1.textContent = '1';
-
-  const elem4 = document.createElement('li');
-  elem4.textContent = '4';
-
-  const elem6 = document.createElement('li');
-  elem6.textContent = '6';
-
-  const elem8 = document.createElement('li');
-  elem8.textContent = '8';
-
-  list.prepend(elem1);
-  document.querySelector('.special').before(elem4);
-  document.querySelector('.special').after(elem6);
-  list.append(elem8);
+const checkboxInput = document.querySelector('.task-status');
+const handleChange = event => {
+  console.log(event.target.checked);
 };
+checkboxInput.addEventListener('change', handleChange);
