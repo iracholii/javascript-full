6949a33df1f5891b407c8be3b14e56a4abe6f6d6
iracholii <ira.cholii.m@gmail.com>
2022-04-26 17:57:40 +0300

    adding

--- lesson22/task6/index.js
+++ lesson22/task6/index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.single-use-btn');
 const buttonFunk = () => {
   console.log('clicked');
-  button.remove('click', buttonFunk);
+  button.removeEventListener('click', buttonFunk);
 };
 button.addEventListener('click', buttonFunk);
