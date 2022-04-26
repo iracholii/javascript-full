133df3d7481b25b47a8be794bcbbe577e05e86f6
iracholii <ira.cholii.m@gmail.com>
2022-04-26 17:14:01 +0300

    adding

--- lesson22/task4/index.js
+++ lesson22/task4/index.js
@@ -1,20 +1,5 @@
-export const getTitle = () => {
-  const elem = document.querySelector('.title').textContent;
-
-  return elem;
+const checkbox = document.querySelector('.task-status');
+const checkboxIfChecked = () => {
+  console.log(checkbox.checked);
 };
-
-export const getDescription = () => {
-  const elem = document.querySelector('.about').innerText;
-  return elem;
-};
-
-export const getPlans = () => {
-  const elem = document.querySelector('.plans').innerHTML;
-  return elem;
-};
-
-export const getGoal = () => {
-  const elem = document.querySelector('.goal').outerHTML;
-  return elem;
-};
+checkbox.addEventListener('change', checkboxIfChecked);
