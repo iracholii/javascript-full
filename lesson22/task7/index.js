2b38f7a6c9988204fa316307a0f23c3c03882f31
iracholii <ira.cholii.m@gmail.com>
2022-04-26 18:05:33 +0300

    adding

--- lesson22/task7/index.js
+++ lesson22/task7/index.js
@@ -1,4 +1,8 @@
-export const clearList = () => {
-  const elem = document.querySelector('.categories');
-  elem.innerHTML = '';
-};
+const buttons = document.querySelectorAll('.btn');
+[...buttons].map(button => {
+  const handleClick = () => {
+    console.log(event.target.textContent);
+  };
+  button.addEventListener('click', handleClick);
+  return button;
+});
