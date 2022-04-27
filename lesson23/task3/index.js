63b85023dabde525b0ca7b24e2140e93b1413196
iracholii <ira.cholii.m@gmail.com>
2022-04-27 22:39:24 +0300

    adding

--- lesson23/task3/index.js
+++ lesson23/task3/index.js
@@ -11,35 +11,64 @@ const listElem = document.querySelector('.list');
 const renderTasks = tasksList => {
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done }) => {
+    .map(element => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = element.done;
 
-      checkbox.checked = done;
+      ///
 
+      const idGenerator = () => {
+        const id = Math.random().toString().slice(2, 10);
+        return id;
+      };
+      checkbox.setAttribute('data-id', idGenerator());
       const onChangeHandler = event => {
-        // const checkboxId = event.target.dataset.id;
+        const checkboxId = event.target.dataset.id;
         if (event.target.checked) {
           event.target.closest('.list__item').classList.add('list__item_done');
+          element.done = true;
         }
         if (!event.target.checked) {
           event.target.closest('.list__item').classList.remove('list__item_done');
+          element.done = false;
+          console.log(tasksList);
         }
       };
       checkbox.addEventListener('change', onChangeHandler);
 
+      ///
+
       checkbox.classList.add('list__item-checkbox');
-      if (done) {
+      if (element.done) {
         listItemElem.classList.add('list__item_done');
       }
-      listItemElem.append(checkbox, text);
+      listItemElem.append(checkbox, element.text);
 
       return listItemElem;
     });
 
   listElem.append(...tasksElems);
+
+  ///
+
+  const createButtonElem = document.querySelector('.create-task-btn');
+
+  const onCreateClickHandler = () => {
+    const taskElem = document.querySelector('.task-input');
+    if (!taskElem.value) {
+      return;
+    }
+    tasksList.push({ text: taskElem.value, done: false });
+    listElem.innerHTML = '';
+    renderTasks(tasksList);
+    taskElem.value = null;
+  };
+
+  createButtonElem.addEventListener('click', onCreateClickHandler);
+  ///
 };
 
 renderTasks(tasks);
@@ -52,40 +81,3 @@ renderTasks(tasks);
 // 3. if Create button clicked - get text from input (.task-input),
 // add it to the tasks array and clear the (.task-input) field
 // 4. renew to-do list
-
-const createButtonElem = document.querySelector('.create-task-btn');
-
-const onCreateClickHandler = () => {
-  const taskElem = document.querySelector('.task-input');
-  if (!taskElem.value) {
-    return;
-  }
-  tasks.push({ text: taskElem.value, done: false });
-  listElem.innerHTML = '';
-  renderTasks(tasks);
-  taskElem.value = null;
-};
-
-createButtonElem.addEventListener('click', onCreateClickHandler);
-
-// const listItemCheckboxArr = [...document.querySelectorAll('.list__item-checkbox')];
-
-const idGenerator = () => {
-  const id = Math.random().toString().slice(2, 10);
-  return id;
-};
-
-// listItemCheckboxArr.map(checkbox => {
-//   checkbox.setAttribute('data-id', idGenerator());
-
-//   const onChangeHandler = event => {
-//     // const checkboxId = event.target.dataset.id;
-//     if (event.target.checked) {
-//       event.target.closest('.list__item').classList.add('list__item_done');
-//     }
-//     if (!event.target.checked) {
-//       event.target.closest('.list__item').classList.remove('list__item_done');
-//     }
-//   };
-//   checkbox.addEventListener('change', onChangeHandler);
-// });
